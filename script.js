891b84e7d9941cbb48e89a524e68cac932d901ff
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-05-15 14:57:25 +0000

    problem

--- script.js
+++ script.js
@@ -14,16 +14,6 @@ function main(){
  * when loop's done, alert the list
  * \n = line brake
  */
- function makeList(){
- let shopArray = [];
-
-let shoppingList = "to buy: \n";
-let shoparray = [];
-for(let item = 1; item<= 5; item++){
-   shopArray.push(prompt("item" + item ));
- }
- alert( shoppingList + shopArray.join("\n") );
-}
 /* 2. for loop and array with toString */
 
 /* Same function, using previous code, BUT:
@@ -41,8 +31,34 @@ for(let item = 1; item<= 5; item++){
  * the at end, alert list with array.join() method with line breaks 
  * instead of commas */
  
-/* 4. loops, arrays and functions */
+/* 4. 2D Arrays: add Pricing */
 
-/* Same function, using previous code, but THIS TIME:
- * store each item as a numbered item ["1. cookies","2. donuts"])   
- * using the loop index. */
+/* Same function, using previous code, but THIS TIME add prices
+ * When someone enters an item, prompt them also to enter a price separating by comma (like milk, 1.99)])   
+ * Convert that entry to an itemPrice array by using string.split() and use a comma as delimiter
+ * Push the itemPrice array to shopArray
+ */
+/* 5. Total up the prices */
+
+/* Same function, using previous code, but THIS TIME total the prices.
+ * After you have built the array, create a NEW loop that adds the price 
+ * of each item to a total. Do this before the alert. 
+ * Save the first alert content as message. 
+ * Add total as a line below message, then alert the message. 
+ */
+function makeList(){
+  let next = "";
+  let total = 0;
+  let shoppingList = "to buy: \n";
+  let shopArray = [];
+  for(let item = 0; item<= 2; item++){
+    next = prompt("item" + item + ":Enter item name , price: milk 1.99" );
+    shopArray.push(next.split(","));
+  }
+  for(let item = 0; item<= 2; item++){
+    total += shopArray[item][1];
+  }
+  let message = ( shoppingList + shopArray.join("\n") );
+  message += "Total is"  + total;
+  alert(message);
+}
